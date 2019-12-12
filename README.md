82769fb1090bb4fb1ea12a73b18b7e18ca74a77a
ben <info@bzjb.com>
2019-12-12 15:57:24 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# raspzerow
-Initial configuration of WIFI Raspberry Pi
+# 树莓派Zero W配置
+WIFI版树莓派配置
+应用环境：wifi版树莓派ZeroW，没有网线，没有全尺寸HDMI，初始化后，通过一个配置文件来自动加载WIFI，以及Frp。因为用到PHP项目，所以顺便用php写了一个简单的配置脚本
+
+1. 下载官方镜像（略）
+2. 烧卡（略）
+3. 加载Boot分区
+    在config.txt末尾添加一行
+    `dtoverlay=dwc2`
+    在cmdline.txt文件中"rootwait"后添加参数
+    `modules-load=dwc2,g_ether`
+
+    在跟目录下建立名为ssh的空文件夹用以开启ssh访问
+
+5. 把卡插入树莓派加电启动
+
+6. 用USB连接电脑，会识别为RNDIS设备
+
+7. 此时可以通过ssh连接树莓派了
+
+    `ssh pi@raspberrypi.local`
+
+    默认密码：raspberry
+    
+
+* * *
+
+安装PHP后可以使用此脚本加载配置
+将init.php复制到/home/pi/init.php
+
+编辑 rc.local
+加入一行
+`/usr/bin/php /home/pi/init.php`
---
+++ init.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * 说明，通过boot分区配置文件的形式实现方便的初始化配置
+ * wifi.txt 配置wifi密码
+ * frp.txt 配置
+ */
+//检查WIFI配置
+$wifistr = @file_get_contents('/boot/wifi.txt');
+if ($wifistr) {
+    $ar = explode("\n", $wifistr);
+    if (count($ar) >= 2) {
+        $sid = trim($ar[0]);
+        $key = trim($ar[1]);
+        $rar[] = "ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev";
+        $rar[] = "update_config=1";
+        $rar[] = "network={";
+        $rar[] = "ssid=\"$sid\"";
+        $rar[] = "psk=\"$key\"";
+        $rar[] = "}";
+        file_put_contents('/etc/wpa_supplicant/wpa_supplicant.conf', join(PHP_EOL, $rar));
+        unlink('/boot/wifi.txt');
+    }
+}
+//检查frp端口
+$frpstr = @file_get_contents('/boot/frp.txt');
+if ($frpstr) {
+    $port = trim($frpstr);
+    $frpfile = @file_get_contents('/opt/frp/frpc.ini');
+    $frpfile = str_replace(['ssh8922', 'http8980'], ['ssh' . $port . '22]', 'http' . $port . '80'], $frpfile);
+    file_put_contents('/opt/frp/frpc.ini', $frpfile);
+    unlink('/boot/frp.txt');
+}
